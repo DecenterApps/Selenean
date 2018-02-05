95c76a53c793b7e42ab4b3be9682da22b5a811c6
Nenad Palinkasevic <nesa993@gmail.com>
2018-02-05 11:25:56 +0100

    Updated PlayerStats contract with implemented ELO

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,12 +1,14 @@
-const Battle = artifacts.require("./Battle.sol");
-const PlayerStats = artifacts.require("./PlayerStats.sol");
 const DecenterCards = artifacts.require("./DecenterCards.sol");
 const Booster = artifacts.require("./Booster.sol");
 const CardMetadata = artifacts.require("./CardMetadata.sol");
 
+const PlayerStats = artifacts.require("./PlayerStats.sol");
+const Battle = artifacts.require("./Battle.sol");
+
 module.exports = function(deployer) {
-  deployer.deploy(Battle);
-  deployer.deploy(PlayerStats);
   deployer.deploy(CardMetadata);
   deployer.deploy(DecenterCards);
+
+  deployer.deploy(Battle);
+  deployer.deploy(PlayerStats);
 };
