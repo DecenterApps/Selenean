2c3984733bd3c806d6414955c472b41ac4da0735
madjarevicn <nikola.madjarevic@gmail.com>
2018-04-02 15:20:00 +0200

    Add config file. Update truffle.js to listen to kovan network.

--- solidity/truffle.js
+++ solidity/truffle.js
@@ -1,11 +1,16 @@
+const dotenv           = require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
+
+
+const mnemonic = process.env.ETHEREUM_ACCOUNT_MNEMONIC;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
     networks: {
-		development: {
-			host: "127.0.0.1",
-			port: 9545,
-			network_id: "*" // Match any network id
-		}
-	}
-};
+        kovan: {
+            provider: function() {
+                return new HDWalletProvider(mnemonic, `https://kovan.infura.io/`);
+            },
+            network_id: '42'
+        }
+    }
+}
