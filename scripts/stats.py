dffc6c278987a7cc32b7252d316e2583ad337a97
Nikola Klipa <klipa.nikola@gmail.com>
2018-07-16 10:57:43 +0200

    Stats script updated

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,59 +2,44 @@ from web3 import Web3, HTTPProvider, IPCProvider
 import json
 import sys
 import texttable as tt
-
-with open('config.json') as data_file:
-    config = json.load(data_file)
+import requests
 
 with open('big.json') as data_file:
 	big = json.load(data_file)
 
-command = sys.argv[1]
-
 names = {}
 for obj in big["cards"]:
-	names[obj["1"]["ID"]] = obj["1"]["title"]
+	names[big["cards"][obj]["1"]["ID"]] = big["cards"][obj]["1"]["title"]
+
+r = requests.get('http://138.68.155.82:8088/cards-owned')
+all_users = json.loads(r.text)
 
 exclude_addresses = ["0xc5B4cb444F3f02E0618F269c03B18D55F90C33E6", "0x6c5a25DCb59781BbAf14CEfacE3093e382c29E23", "0xA5Fdb57fEd1DFA68557e88C3Ba3786A9Ca8Fa518", "0xd8E3ec2bD3FC6C02F811dE188B7bF75A206d2CBA", "0xf3FE977d431FA3d723bcB6e4b77f6B1884F7D94C","0x2582EEde06784951E963687bc0bC1361d49f2b8A", "0X6C259EA1FCA0D1883E3FFFDDEB8A0719E1D7265f", "0xA2CC37dEb2C1d171Cf4FAaE2C8b94F64d65e3a83", "0x00158A74921620b39E5c3aFE4dca79feb2c2C143", "0x93cdB0a93Fc36f6a53ED21eCf6305Ab80D06becA"]
 exclude_addresses = [a.upper() for a in exclude_addresses]
 
-web3 = Web3(HTTPProvider('https://kovan.decenter.com'))
-card_abi = config['cardContract']['abi']
-card_address = config['cardContract']['address']
-card_contract = web3.eth.contract(card_address, abi=card_abi)
 
-numOfCards = card_contract.call().totalSupply()
-
-if command == "min":
-	print("Num of cards: {}".format(numOfCards))
-	print("Num of boosters: {}".format(numOfCards/5))
-	sys.exit()
-
-mapping = {}
-players = []
-cards_by_player = {}
 total_cards = 0
-for i in range(0, numOfCards):
-	owner = card_contract.call().ownerOf(i).upper()
-	if owner in exclude_addresses:
-		continue
-	
-	total_cards += 1
-	
-	if owner in players:
-		cards_by_player[owner] += 1
-	else:
-		cards_by_player[owner] = 1
-		players.append(owner)
-	# ------
-	meta = card_contract.call().metadata(i)[0]
+all_cards = {}
+cards_by_player = {}
+for user in all_users:
+	#if user['address'].upper() in exclude_addresses:
+	#	continue
+	user_cards = 0
+	count = 0
+	for card in user['cards']:
+		if card>0:
+			user_cards += card
+			name = names[str(count)]
+			if name in all_cards:
+				all_cards[name] += card
+			else:
+				all_cards[name] = card 
+		count += 1
 
-	if names[str(meta)] in mapping:
-		mapping[names[str(meta)]] += 1
-	else:
-		mapping[names[str(meta)]] = 1
+	cards_by_player[user['address']] = user_cards
+	total_cards += user_cards 
 
-result = sorted(mapping.items() , key=lambda t : t[1])
+result = sorted(all_cards.items() , key=lambda t : t[1])
 
 tab = tt.Texttable()
 headings = ['Card','Num']
